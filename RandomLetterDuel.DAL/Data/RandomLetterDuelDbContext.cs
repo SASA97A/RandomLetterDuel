f57d5ba586b6f9b40f73e9c4c901acf1dda63843
SASA97A <safarahmad@hotmail.com>
2026-05-09 17:39:20 +0200

    DataDB and Entities are done

---
+++ RandomLetterDuel.DAL/Data/RandomLetterDuelDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RandomLetterDuel.DAL.Data
+{
+    public class RandomLetterDuelDbContext : DbContext
+    {
+        public RandomLetterDuelDbContext(DbContextOptions<RandomLetterDuelDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Entities.GameRoomEntity> GameRooms { get; set; }
+        public DbSet<Entities.PlayerEntity> Players { get; set; }
+    }
+}
---
+++ RandomLetterDuel.DAL/Entities/PlayerEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RandomLetterDuel.DAL.Entities
+{
+    public class PlayerEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public int Score { get; set; }
+
+        public Guid GameRoomId { get; set; }
+    }
+}
